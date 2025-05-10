b44e58f6c5c14fc84f4cb886911baeb708d9d20f
JosefAmbruz <ambruz.josef03@gmail.com>
2025-05-10 17:57:15 +0200

    Merged latest updates to interpret into the dedicated feature/interpret branch

---
+++ src/build/Desktop-Debug/nodeeditor_build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/josef/Documents/FITVUT/2BIT-letní/ICP/Projekt/ICP-projekt/src/nodeeditor-master
+# Build directory: /home/josef/Documents/FITVUT/2BIT-letní/ICP/Projekt/ICP-projekt/src/build/Desktop-Debug/nodeeditor_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external")
