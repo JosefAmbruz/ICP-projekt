ae41235b9264e59273816881873f6d4e347b843c
JosefAmbruz <ambruz.josef03@gmail.com>
2025-05-11 21:11:55 +0200

    Fixed empty action code

--- src/build/Desktop-Debug/interpret/output.py
+++ src/build/Desktop-Debug/interpret/output.py
@@ -8,39 +8,15 @@ import logging
 # --- Define FSM Actions and Conditions ---
 
 def action_State_1(fsm, variables):
-    x = variables.get('x')
-    
-    print("In State 1")
-    fsm.set_variable('x', x)
-    
+    pass
 
 
 def action_State_2(fsm, variables):
-    x = variables.get('x')
-    
-    print("In State 2")
-    x += 1
-    print(x)
-    
-    fsm.set_variable('x', x)
-    
-
-
-def action_State_Final(fsm, variables):
-    x = variables.get('x')
-    
-    print("In State Final")
-    print(x)
-    fsm.set_variable('x', x)
-    
+    pass
 
 
 def condition_always_true(fsm, variables):
     return True
-
-
-def condition_x_gt_2(fsm, variables):
-    return (variables.get('x') > 2)
 
 
 # --- Main FSM Execution ---
@@ -61,42 +37,23 @@ if __name__ == "__main__":
         is_start_state=False,
         is_finish_state=False
     )
-    state_State_Final = State(
-        name="State Final",
-        action=action_State_Final,
-        is_start_state=False,
-        is_finish_state=True
-    )
 
     # 3. Define Transitions
-    tr_State_1_to_State_Final_0 = Transition(
-        target_state_name="State Final",
-        condition=condition_x_gt_2,
-        delay=1000.0
-    )
-    tr_State_2_to_State_1_1 = Transition(
-        target_state_name="State 1",
-        condition=condition_always_true,
-        delay=1000.0
-    )
-    tr_State_1_to_State_2_2 = Transition(
+    tr_State_1_to_State_2_0 = Transition(
         target_state_name="State 2",
         condition=condition_always_true,
-        delay=1000.0
+        delay=0.0
     )
 
     # 4. Add Transitions to States
-    state_State_1.add_transition(tr_State_1_to_State_Final_0)
-    state_State_2.add_transition(tr_State_2_to_State_1_1)
-    state_State_1.add_transition(tr_State_1_to_State_2_2)
+    state_State_1.add_transition(tr_State_1_to_State_2_0)
 
     # 5. Add States to FSM
     my_fsm.add_state(state_State_1)
     my_fsm.add_state(state_State_2)
-    my_fsm.add_state(state_State_Final)
 
     # 6. Set Initial Variables
-    my_fsm.set_variable("x", 0)
+    # No initial variables defined in specification.
 
     # 7. Connect to client and Run the FSM
     client_host = 'localhost'
